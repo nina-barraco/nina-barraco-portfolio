7b4ed3282622563f3e9a10ece0a8d2e43ef84f26
nina-barraco <barraconina@gmail.com>
2019-02-19 22:36:41 -0500

    commem page and images

--- portfolio.js
+++ portfolio.js
@@ -1,5 +1,11 @@
+// function clickNav(x) {
+//     x.classList.toggle("change");
+//     var display = document.getElementById("topnav-links-js");
+//     display.classList.toggle("display-links");
+//   }
+
 function clickNav(x) {
-    x.classList.toggle("change");
-    var display = document.getElementById("topnav-links-js");
-    display.classList.toggle("display-links");
-  }
+  x.classList.toggle("change");
+  var display = document.getElementById("topnav-links-js");
+  display.classList.toggle("display-links");
+}
