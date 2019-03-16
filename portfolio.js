e8498f4fa3384b8fba8555baa4f46df8c14a523d
nina-barraco <barraconina@gmail.com>
2019-03-16 16:55:11 -0400

    added gbo page

--- portfolio.js
+++ portfolio.js
@@ -9,3 +9,17 @@ function clickNav(x) {
   var display = document.getElementById("topnav-links-js");
   display.classList.toggle("display-links");
 }
+
+function fadeIn(x) {
+  var fadeElements = Array.from(document.getElementsByClassName("fade-js"));
+  var windowHeight = window.innerHeight;
+  function checkPosition() {
+    for (var i = 0; i < fadeElements.length; i++) {
+      var positionFromTop = fadeElements[i].getBoundingClientRect().top;
+      if (positionFromTop - windowHeight <= 0) {
+        fadeElements[i].classList.add("fade-animation");
+      }
+    }
+  };
+  checkPosition();
+}
